246e712a2c593294c6c90b5f06d44ac92e220147
Project_IO <me@projecttl.net>
2023-12-22 12:56:12 +0900

    feat: done

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("fabric-loom")
+    `maven-publish`
+    java
+}
+
+group = property("mod_group")!!
+version = property("mod_version")!!
+
+val minecraft_version: String by project
+val yarn_mappings: String by project
+val loader_version: String by project
+
+val fabric_version: String by project
+val fabric_kotlin_version: String by project
+
+repositories {
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    minecraft("com.mojang:minecraft:${minecraft_version}")
+    mappings("net.fabricmc:yarn:${yarn_mappings}:v2")
+
+    modImplementation("net.fabricmc:fabric-loader:${loader_version}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabric_version}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${fabric_kotlin_version}")
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") {
+            expand(getProperties())
+            expand(mutableMapOf("version" to project.version))
+        }
+    }
+
+    jar {
+        from("LICENSE")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                artifact(remapJar) {
+                    builtBy(remapJar)
+                }
+                artifact(kotlinSourcesJar) {
+                    builtBy(remapSourcesJar)
+                }
+            }
+        }
+
+        repositories {
+        }
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+java {
+    withSourcesJar()
+}
---
+++ src/main/kotlin/net/projecttl/mod/example/ExampleMod.kt
@@ -0,0 +1,9 @@
+package net.projecttl.mod.example
+
+import net.fabricmc.api.ModInitializer
+
+class ExampleMod : ModInitializer {
+    override fun onInitialize() {
+        TODO("Not yet implemented")
+    }
+}
